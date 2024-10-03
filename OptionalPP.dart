963d0c9c39c9b5604a084a5f6f3e81889065fdb0
Dong Min <focuspdm4383@naver.com>
2024-10-03 12:30:26 +0900

    처음커밋

--- OptionalPP.dart
+++
@@ -1,8 +0,0 @@
-String sayHello(String name, int age , [String?
-country = "한국"]) =>
-  "안녕 내이름은 $name 이고 $age 살이야 국적은 $country 야";
-
-  void main(){
-    var results = sayHello("철수",23);
-    print(sayHello);
-  }
